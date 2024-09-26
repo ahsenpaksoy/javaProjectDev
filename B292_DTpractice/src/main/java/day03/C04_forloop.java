2571970ad9654f1bbf1eb8ab996d0caf8674a3b2
ahsenpaksoy <ahsenpaksoy@gmail.com>
2024-09-26 21:34:40 +0200

    adding code

--- B292_DTpractice/src/main/java/day03/C04_forloop.java
+++ B292_DTpractice/src/main/java/day03/C04_forloop.java
@@ -22,6 +22,15 @@ public class C04_forloop {
             System.out.println();
         }
 
+       // ODEV
+        /*
+                   *
+                  * *
+                 *****
+                *     *
+               *       *
+         */
+
 
 
     }
