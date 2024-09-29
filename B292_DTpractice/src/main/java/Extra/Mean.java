a59e3c0b28c525dfb69cd0f1216e75e3454745a5
ahsenpaksoy <ahsenpaksoy@gmail.com>
2024-09-29 13:05:37 +0200

    Array practice

---
+++ B292_DTpractice/src/main/java/Extra/Mean.java
@@ -0,0 +1,20 @@
+package Extra;
+
+public class Mean {
+
+    public static void main(String[] args) {
+        int[] numbers = {5,5,5,5,4};
+        int sum =0;
+        double res = 0.0;
+        for(int w:numbers){
+            sum = sum + w;
+        }
+
+        res = (double) sum/numbers.length;
+        System.out.println("Mean:"+res);
+
+        System.out.println((double)5/3);
+
+    }
+
+}
