a16a97a82090420d06b7b894b33ab1d23cbceeb5
ahsenpaksoy <ahsenpaksoy@gmail.com>
2024-10-03 21:26:51 +0200

    Arrays practice

---
+++ B292_DTpractice/src/main/java/day04/C04_arrays.java
@@ -0,0 +1,13 @@
+package day04;
+
+public class C04_arrays {
+    public static void main(String[] args) {
+
+                /*
+            4.soru Verilen bir int array'deki
+            en buyuk sayiyi yazdiran bir method olusturun.
+        */
+
+
+    }
+}
