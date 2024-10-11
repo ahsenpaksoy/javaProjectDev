dc570dfdf6ae3e85a8192db142192098ad7285ac
ahsenpaksoy <ahsenpaksoy@gmail.com>
2024-10-11 23:33:32 +0200

    OOP

---
+++ Batch291NT/src/main/java/d22oop/TestClass.java
@@ -0,0 +1,9 @@
+package d22oop;
+
+public class TestClass {
+    public static void main(String[] args) {
+
+        ChildClass obj = new ChildClass();
+        System.out.println(obj.multiply(5, 4)); //30  Bu cagrı bi uste yapildi cunku obje ChildClass dan olusturuldu
+    }
+}
