9a534abb7199d60d18efe0928c5d4dd0f3c25e25
ahsenpaksoy <ahsenpaksoy@gmail.com>
2024-10-12 22:02:59 +0200

    InterviewQuestion

---
+++ B292_DTpractice/src/main/java/interviewday01/C05_InterviewQuestion.java
@@ -0,0 +1,15 @@
+package interviewday01;
+
+public class C05_InterviewQuestion {
+    public static void main(String[] args) {
+
+        /*
+        Tüm alfabeyi loop kullanarak yazdiriniz
+         */
+
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            System.out.print( ch +" "); }
+
+
+    }
+}
