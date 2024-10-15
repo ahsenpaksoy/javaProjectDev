71f42b630df2faab14a70b5e69752f28a2057dad
ahsenpaksoy <ahsenpaksoy@gmail.com>
2024-10-15 22:41:11 +0200

    Abstraction

---
+++ Batch291NT/src/main/java/d23oop_collections/AbstractChild.java
@@ -0,0 +1,8 @@
+package d23oop_collections;
+
+public abstract class AbstractChild extends Honda{
+
+    //Bu abstract bir class'tir. Parent'i da abstract.
+    //Parent'inin abstract methodlarini implement etmek zorunda degil
+
+}
