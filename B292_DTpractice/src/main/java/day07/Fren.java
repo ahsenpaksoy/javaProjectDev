be0f306fd5203e13ff84d036f6fb4a4e4008d4af
ahsenpaksoy <ahsenpaksoy@gmail.com>
2024-10-26 21:33:10 +0200

    interface

---
+++ B292_DTpractice/src/main/java/day07/Fren.java
@@ -0,0 +1,13 @@
+package day07;
+
+public interface Fren {
+    //fren abs fren mi oldugunu kontrol eder
+    boolean isAbs();
+
+    String getYagDurumu();
+
+    void servoFrenUygula();
+
+    void motorFreniUygula();
+
+}
