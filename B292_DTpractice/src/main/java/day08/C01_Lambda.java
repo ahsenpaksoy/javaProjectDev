2a2e6ce76f5c31810e0aae1e8c7ffb19d7b64b6f
ahsenpaksoy <ahsenpaksoy@gmail.com>
2024-11-02 19:59:46 +0100

    Lambda

---
+++ B292_DTpractice/src/main/java/day08/C01_Lambda.java
@@ -0,0 +1,19 @@
+package day08;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class C01_Lambda {
+    public static void main(String[] args) {
+
+        //1) bir listedeki elemanlardan 5'e tam bolunenleri yazdiran bir method olusturun
+
+        List<Integer> mylist=new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11));
+        beseBolunen(mylist);
+    }
+
+    private static void beseBolunen(List<Integer> myList) {
+        myList.stream().filter(t -> t%5==0).forEach(System.out::println);
+    }
+}
